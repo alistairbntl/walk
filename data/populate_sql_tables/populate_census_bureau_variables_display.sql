bddf677a89584f5d8329034d110d36adfc2dc703
Alistair Bentley <alistairbntl@gmail.com>
2024-12-04 20:24:53 -0500

    update census_buereau_variables

--- data/populate_sql_tables/populate_census_bureau_variables_display.sql
+++ data/populate_sql_tables/populate_census_bureau_variables_display.sql
@@ -1,5 +1,28 @@
 CREATE TABLE census_bureau_variables_display (
     variable_id text,
-    display_namme text,
-    dtype text
-)
+    display_name text,
+    dtype text,
+    default_ts_transformation text
+);
+
+insert into census_bureau_variables_display
+select
+    'B19013_001E'
+    , 'median_household_income'
+    , 'int'
+    , 'pct_chg';
+
+insert into census_bureau_variables_display
+select
+    'B01001_001E'
+    , 'total_population'
+    , 'int'
+    , 'pct_chg';
+
+insert into census_bureau_variables_display
+select
+    'B01002_001E'
+    , 'median_age'
+    , 'float'
+    , 'diff';
+
