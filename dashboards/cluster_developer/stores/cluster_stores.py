9f452042b47cbb885108bb82ce950b8d45669b90
Alistair Bentley <alistairbntl@gmail.com>
2025-03-25 20:39:30 -0400

    update cluster stores

---
+++ dashboards/cluster_developer/stores/cluster_stores.py
@@ -0,0 +1,9 @@
+from dash import dcc
+
+cluster_stores = [
+    dcc.Store(id="cluster-geodata"),
+    dcc.Store(id="cluster-plotdata"),
+    dcc.Store(id="cluster-metadata-level", data={"geolevel": "county"}),
+    dcc.Store(id="cluster-columns", data=[]),
+    dcc.Store(id="cluster_results"),
+]
